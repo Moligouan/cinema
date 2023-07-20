8d082325f9a34abbee9aa12dc7f93a3aaef3923c
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-20 13:41:39 +0200

    Add files via upload

---
+++ controller/offres.php
@@ -0,0 +1,11 @@
+<?php
+
+//On appelle la fonction getAll()
+$offresDao = new OffresDAO();
+
+$offers = $offresDao->getAll();
+
+//On affiche le template Twig correspondant
+echo $twig->render('offres.html.twig', [
+    'offers' => $offers
+]);
