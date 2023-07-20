470f2c2fb98e440b8c1e7630b13b95979ee3914f
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-20 13:41:55 +0200

    Add files via upload

---
+++ model/DAL/userDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class UserDAO extends Dao
+{
+
+    public function getAll()
+    {
+    }
+
+    public function add($user)
+    {
+    }
+
+    //Récupérer plus d'info sur 1 offre
+    public function getOne($id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
