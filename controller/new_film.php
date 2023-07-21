ee19ce0a923b0b2f2ee7957f4d358f7669f35365
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-21 15:08:49 +0200

    Update and rename creer_offre.php to new_film.php

---
+++ controller/new_film.php
@@ -0,0 +1,35 @@
+<?php
+
+$filmDao = new FilmDAO();
+
+// $film = new Film($_POST['id'], $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
+
+// $status = $filmDao->add($film);
+// $message = "";
+$nfilm = null;
+try {
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["titre"]) && isset($_POST["realisateur"]) && isset($_POST["affiche"]) && isset($_POST["annee"])) {
+        $offre = new Film(null, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']); // nom de l'offre et description
+        $status = $filmDao->add($offre);
+        if ($status) {
+            $message =  "Ajout OK";
+        } else {
+            $message = "Erreur Ajout";
+        }
+    }
+} catch (Exception $err) {
+    $message = "ERROR : " . $err->getMessage();
+}
+
+// if ($status) {
+//     $message =  "Ajout OK";
+// } else {
+//     $message = "Erreur Ajout";
+// }
+
+
+echo $twig->render('creer_film.html.twig', [
+    'message' => $message,
+    'film' => $nfilm
+]);
