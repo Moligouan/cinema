1c0b114644e2eea67833a3228e2104b751bbfd16
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-21 13:37:28 +0200

    Add files via upload

---
+++ model/BO/Film.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Offres
+ *
+ * @author Vince
+ */
+class Film
+{
+
+    private $id;
+    private $titre;
+    private $realisateur;
+    private $affiche;
+    private $annee;
+    private array $role = [];
+
+    public function __construct($id, $titre, $realisateur, $affiche, $annee, array $role = [])
+    {
+        $this->setId($id);
+        $this->setTitre($titre);
+        $this->setRealisateur($realisateur);
+        $this->setAffiche($affiche);
+        $this->setAnnee($annee);
+        $this->setRole($role);
+    }
+
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of titre
+     */ 
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Get the value of realisateur
+     */ 
+    public function getRealisateur()
+    {
+        return $this->realisateur;
+    }
+
+    /**
+     * Get the value of affiche
+     */ 
+    public function getAffiche()
+    {
+        return $this->affiche;
+    }
+
+    /**
+     * Get the value of annee
+     */ 
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+    // --------------------------------------
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of titre
+     *
+     * @return  self
+     */ 
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of realisateur
+     *
+     * @return  self
+     */ 
+    public function setRealisateur($realisateur)
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of affiche
+     *
+     * @return  self
+     */ 
+    public function setAffiche($affiche)
+    {
+        $this->affiche = $affiche;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of annee
+     *
+     * @return  self
+     */ 
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+}
---
+++ model/BO/Role.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Offres
+ *
+ * @author Vince
+ */
+class Role
+{
+
+    private $id;
+    private $personnage;
+    private $nom;
+    private $prenom;
+
+
+    public function __construct($id, $personnage, $nom, $prenom)
+    {
+        $this->setId($id);
+        $this->setPersonnage($personnage);
+        $this->setNom($nom);
+        $this->setPrenom($prenom);
+    }
+
+    
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of personnage
+     */ 
+    public function getPersonnage()
+    {
+        return $this->personnage;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of personnage
+     *
+     * @return  self
+     */ 
+    public function setPersonnage($personnage)
+    {
+        $this->personnage = $personnage;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
