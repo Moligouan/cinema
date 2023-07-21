77930d05a076599b2b34438bb31349b1bf139bea
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-21 15:07:03 +0200

    Update Film.php

--- model/BO/Film.php
+++ model/BO/Film.php
@@ -134,4 +134,26 @@ class Film
 
         return $this;
     }
+
+// -----------------------------------
+
+    /**
+     * Get the value of role
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
 }
