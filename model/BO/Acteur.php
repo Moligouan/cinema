5b068670655e2552937de8e9643ceaf4c4a48984
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-24 14:15:40 +0200

    Add files via upload

---
+++ model/BO/Acteur.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Offres
+ *
+ * @author Vince
+ */
+class Acteur
+{
+
+    private $id;
+    private $nom;
+    private $prenom;
+
+    public function __construct(int $id, string $nom, string $prenom)
+    {
+        $this->setId($id);
+        $this->setNom($nom);
+        $this->setPrenom($prenom);
+    }
+
+    
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
