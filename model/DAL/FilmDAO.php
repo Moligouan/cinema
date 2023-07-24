e3395f6ef54baed31eb63f34ae69c3749e8adbb7
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-24 13:56:53 +0200

    Update FilmDAO.php

--- model/DAL/FilmDAO.php
+++ model/DAL/FilmDAO.php
@@ -7,12 +7,12 @@ class FilmDAO extends Dao
     public function getAll()
     {
 
-        $query = $this->BDD->prepare("SELECT id, titre, realisateur, affiche, annee  FROM films");
+        $query = $this->BDD->prepare("SELECT idFilm, titre, realisateur, affiche, annee  FROM films");
         $query->execute();
         $offers = array();
 
         while ($data = $query->fetch()) {
-            $film[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
+            $film[] = new Film($data['idFilm'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
         }
         return ($film);
     }
@@ -21,8 +21,8 @@ class FilmDAO extends Dao
     public function add($data)
     {
 
-        $valeurs = ['titre' => $data->getTitre(), 'realisateur' => $data->getRealisateur(), 'affiche' => $data->getAffiche(), 'annee' => $data->getAnnee()];
-        $requete = 'INSERT INTO films (titre, realisateur, affiche, annee) VALUES (:titre , :realisateur, :affiche, :annee)';
+        $valeurs = ['id' => $data->getId(),'titre' => $data->getTitre(), 'realisateur' => $data->getRealisateur(), 'affiche' => $data->getAffiche(), 'annee' => $data->getAnnee()];
+        $requete = 'INSERT INTO films (idFilm, titre, realisateur, affiche, annee) VALUES (:id, :titre , :realisateur, :affiche, :annee)';
         $insert = $this->BDD->prepare($requete);
         if (!$insert->execute($valeurs)) {
             return false;
@@ -35,10 +35,10 @@ class FilmDAO extends Dao
     public function getOne($id)
     {
 
-        $query = $this->BDD->prepare('SELECT * FROM films WHERE films.id = :id_film');
+        $query = $this->BDD->prepare('SELECT * FROM films WHERE films.idFilm = :id_film');
         $query->execute(array(':id_film' => $id));
         $data = $query->fetch();
-        $offer = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
+        $offer = new Film($data['idFilm'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
         return ($offer);
     }
 
