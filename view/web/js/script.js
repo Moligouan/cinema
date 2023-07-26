f91c9b042641b70aa4db0bf3d63de50fd6ff3329
Moligouan <139350006+Moligouan@users.noreply.github.com>
2023-07-26 14:41:59 +0200

    Update script.js

--- view/web/js/script.js
+++ view/web/js/script.js
@@ -5,28 +5,32 @@ function add() {
 
     if (input_tags.length < 3) {
         var newField = document.createElement('div');
+        newField.setAttribute('id', 'ligne ' + (input_tags.length + 1));
         newField.setAttribute('class', 'rolep');
         formfield.appendChild(newField);
 
         var Perso = document.createElement('input');
         Perso.setAttribute('type', 'text');
-        Perso.setAttribute('name', 'personnage');
+        Perso.setAttribute('name', 'personnage_' + (input_tags.length));
         Perso.setAttribute('class', 'perso');
         Perso.setAttribute('placeholder', 'Personnage');
+        Perso.setAttribute('required', '');
         newField.appendChild(Perso);
 
         var Nom = document.createElement('input');
         Nom.setAttribute('type', 'text');
-        Nom.setAttribute('name', 'nom');
+        Nom.setAttribute('name', 'nom_' + (input_tags.length));
         Nom.setAttribute('class', 'name');
         Nom.setAttribute('placeholder', 'Nom');
+        Nom.setAttribute('required', '');
         newField.appendChild(Nom);
 
         var Prenom = document.createElement('input');
         Prenom.setAttribute('type', 'text');
-        Prenom.setAttribute('name', 'prenom');
+        Prenom.setAttribute('name', 'prenom_' + (input_tags.length));
         Prenom.setAttribute('class', 'name');
         Prenom.setAttribute('placeholder', 'Prenom');
+        Prenom.setAttribute('required', '');
         newField.appendChild(Prenom);
 
         var Delete = document.createElement('button');
@@ -41,7 +45,7 @@ function add() {
     }
 }
 
-function remove() {
+function remove(ligne) {
     if (input_tags.length >= 2) {
         formfield.removeChild(input_tags[(input_tags.length) - 1]);
     }
