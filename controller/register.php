2df0e77ecb9f143faacfaf6df42df1c542b2a6d8
JoElKik <140053321+JoElKik@users.noreply.github.com>
2023-07-31 07:55:05 +0200

    Update register.php

--- controller/register.php
+++ controller/register.php
@@ -6,19 +6,22 @@ $msg = "";
 $errMsg = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && (!empty($_POST['email']) && !empty($_POST['pw']) && !empty($_POST['username']))) {
-    // Vérification de l'écriture de l'email
+    // Vérification email valide
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         $msg = "Invalidated email. Please enter a proper one.";
     } else {
         $user = new User(null, $_POST['username'], $_POST['email'], $_POST['pw']);
+        // Test sur email pour verifier nouvel user n'existe pas dans la BDD (check new return booleen)
         $query = $userDAO->checkNew($user);
 
         if ($query) {
             $errMsg = "This email already exists. Please type a different one.";
-        } else if ($_POST['pw'] != $_POST['confirmPW']) {
+        } // Test si mot de passe et confirmation son les mêmes
+        else if ($_POST['pw'] != $_POST['confirmPW']) {
             $msg = "Password not confirmed. Please confirm it correctly.";
         } else {
             $user = $userDAO->add($user);
+             //on definit le session.user après l'avoir ajouter a la BDD
             $_SESSION['user'] = $_POST['username'];
             header("Location: films");
         }
