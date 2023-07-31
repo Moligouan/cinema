69de43804d423282c0a565098ae49dfb6b971613
JoElKik <140053321+JoElKik@users.noreply.github.com>
2023-07-31 08:06:52 +0200

    Update films.php

--- controller/films.php
+++ controller/films.php
@@ -2,13 +2,37 @@
 
 //On appelle la fonction getAll()
 $filmDao = new FilmDAO();
+$roleDao = new RoleDAO();
+$acteurDao = new ActeurDAO();
 
-$listFilms = $filmDao->getAll();
+// fonction recherche
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["recherche"])) {
+    $film = $filmDao->search($_POST["recherche"]);
+}
+else {
+    $film = $filmDao->getAll();
+}
+// $role = $roleDao->getRole($film[0]->getId());
+// $acteur = $acteurDao->getOne($role[0]->getIdAct());
+//test de la fonction qui fait correspondre film avec acteur et role
+$a = 0;
+$b = 0;
+while($b <= (count($film)-1)){
+    $role = $roleDao->getRole($film[$b]->getId());
+    while($a <= (count($role)-1)){
+        $acteur = $acteurDao->getOne($role[$a]->getIdAct());
+        $role[$a]->setActeur($acteur);
+        $film[$b]->addRole($role[$a], $a);
+        $a = $a+1;
+    }
+    $a = $a-$a;
+    $b = $b+1;
+}   
+$test = count($film);
 
-
-
-//On affiche le template Twig correspondant
 echo $twig->render('films.html.twig', [
-    'listFilms' => $listFilms,
+    'films' => $film,
+
+    'count' => $b,
 
 ]);
