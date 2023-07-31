f658820b0aacff018443272e471c509034ec5d5c
JoElKik <140053321+JoElKik@users.noreply.github.com>
2023-07-31 07:54:15 +0200

    Update user.php

--- controller/user.php
+++ controller/user.php
@@ -9,13 +9,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && (isset($_POST['email']) && isset($_P
     $user = new User(null, null, $_POST['email'], $_POST['pw']);
 
     $user = $userDAO->getOne($user);
-
+ // Verifie le type de user apres la fonction getOne qui retourne un type pour user (dans la BDD ,si nombre email pas ok, si null mot de passe pas ok )
     if (gettype($user) == "integer") {
         $msg = "Login error : unknown user";
     } else if ($user == null) {
         $msg = "mot de passe incorrect";
     } else {
-
+//on definit session.user (different de register car on ne recupere pas username dans le formulaire mais directement depuis BDD)
         $_SESSION['user'] = $user->getUsername();
         header("Location: films");
 
